739f5f184b7ac9795422f8fc181b1acf69c9c9b2
Cami Gomez <camigomez@hugeinc.com>
2023-07-04 16:38:46 -0500

    Add multiplayer

--- codebreaker.py
+++ codebreaker.py
@@ -4,7 +4,10 @@ Codebreaker Class
 
 
 class Codebreaker:
-    def __init__(self, hidden_number=None) -> None:
+    def __init__(self) -> None:
+        pass
+
+    def set_hidden_number(self, hidden_numer=None):
         pass
 
     def play(self):
@@ -22,8 +25,8 @@ class Codebreaker:
             attempt += 1
 
             if response == 'XXXX':
-                print('¡Felicitaciones! Has ganado')
-                return
+                print('¡Felicitaciones! Has ganado en {attempt} intentos')
+                return attempt
 
     def guess_number(self, number=None):
         pass
--- main.py
+++ main.py
@@ -1,6 +1,43 @@
+import os
 from codebreaker import Codebreaker
+
+
+def clean():
+    # For Windows
+    if os.name == 'nt':
+        _ = os.system('cls')
+
+    # For macOS and Linux
+    else:
+        _ = os.system('clear')
 
 
 if __name__ == '__main__':
     codebreaker = Codebreaker()
-    codebreaker.play()
+    action = input('Que deséa hacer? (1) Jugar, (2) Salir: ')
+
+    while action == '1':
+        print('Juagdor 1')
+        hidden_number = input('Escriba el número oculto:')
+        codebreaker.set_hidden_number(hidden_number)
+        clean()
+        print('Juagdor 2')
+        attempt_player_2 = codebreaker.play()
+
+        print('--------------------')
+        print('Juagdor 2')
+        hidden_number = input('Escriba el número oculto:')
+        codebreaker.set_hidden_number(hidden_number)
+        clean()
+        print('Juagdor 1')
+        attempt_player_1 = codebreaker.play()
+
+        if attempt_player_1 > attempt_player_2:
+            print('El jugador 1 es el ganador')
+        elif attempt_player_1 < attempt_player_2:
+            print('El jugador 2 es el ganador')
+        else:
+            print('Wow! Es un empate!')
+        
+        print('--------------------')
+        action = input('Que deséa hacer? (1) Jugar de nuevo, (2) Salir: ')
